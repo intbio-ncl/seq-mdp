55da21553d8c5c9199e334622550e1742cc30e32
Christian <c.atallah2@newcastle.ac.uk>
2021-02-09 14:04:34 +0000

    Write 'none' for matches without PFAM ids

--- lib/hmm_annotation.py
+++ lib/hmm_annotation.py
@@ -4,9 +4,9 @@ from tqdm import tqdm
 import requests
 import numpy as np
 
-_PFAMPATH = "./hmmsearch_results/hmmsearch_pfam_sub.tab"
-_MDPPATH = "./hmmsearch_results/hmmsearch_mdp_sub.tab"
-_OVERLAPPATH = "./hmmsearch_results/hmmsearch_overlap_sub.tab"
+_PFAMPATH = "./hmmsearch_results/hmmsearch_pfam_sub_e15.tab"
+_MDPPATH = "./hmmsearch_results/hmmsearch_mdp_sub_e15.tab"
+_OVERLAPPATH = "./hmmsearch_results/hmmsearch_overlap_sub_e15.tab"
 _URL = "https://www.ebi.ac.uk/proteins/api/proteins/"
 
 
@@ -41,7 +41,7 @@ def query_api(res, out):
 
         if flag:
             ann_dict['None'] += [res[id]]
-                # TODO: track avg±std eval per signature (make it a list instead of a count)
+            fw.write(f"{id}\tNone\t{res[id]}\n")
                 #TODO: for overlap, calculate the difference for each accession
 
     return ann_dict
@@ -60,13 +60,13 @@ def main():
     mdp_res = read_file(_MDPPATH)
     overlap_res = read_file(_OVERLAPPATH)
 
-    pfam_ann = query_api(pfam_res, "./hmmsearch_results/pfam_evals.tab")
-    mdp_ann = query_api(mdp_res, "./hmmsearch_results/mdp_evals.tab")
-    overlap_ann = query_api(overlap_res, "./hmmsearch_results/overlap_evals.tab")
+    pfam_ann = query_api(pfam_res, "./hmmsearch_results/pfam_evals_e15.tab")
+    mdp_ann = query_api(mdp_res, "./hmmsearch_results/mdp_evals_e15.tab")
+    overlap_ann = query_api(overlap_res, "./hmmsearch_results/overlap_evals_e15.tab")
 
-    write_sig_res(pfam_ann, "./hmmsearch_results/pfam_sigs.tab")
-    write_sig_res(mdp_ann, "./hmmsearch_results/mdp_sigs.tab")
-    write_sig_res(overlap_ann, "./hmmsearch_results/overlap_sigs.tab")
+    write_sig_res(pfam_ann, "./hmmsearch_results/pfam_sigs_e15.tab")
+    write_sig_res(mdp_ann, "./hmmsearch_results/mdp_sigs_e15.tab")
+    write_sig_res(overlap_ann, "./hmmsearch_results/overlap_sigs_e15.tab")
 
     # [print(x, len(pfam_ann[x])) for x in pfam_ann]
     # [print(x, len(mdp_ann[x])) for x in mdp_ann]
